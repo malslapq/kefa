142468477f9a00f14f61c821443fc3d315e0877a
malslapq <malslapq@gmail.com>
2025-02-17 15:41:00 +0900

    feat: Spring boot 초기 셋팅

---
+++ src/main/java/com/kefa/KefaApplication.java
@@ -0,0 +1,13 @@
+package com.kefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KefaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KefaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kefa/KefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KefaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
