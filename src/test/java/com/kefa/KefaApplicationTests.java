60d03ff7ab7a3cdb1235680214b50c8477e77513
malslapq <malslapq@gmail.com>
2025-02-22 03:22:00 +0900

    test: JPA 관련 Mock처리 h2 db 설정 추가

--- src/test/java/com/kefa/KefaApplicationTests.java
+++ src/test/java/com/kefa/KefaApplicationTests.java
@@ -2,10 +2,18 @@ package com.kefa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.test.context.TestPropertySource;
 
-@ActiveProfiles("test")
 @SpringBootTest
+@MockBean(JpaMetamodelMappingContext.class)
+@TestPropertySource(properties = {
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class KefaApplicationTests {
 
     @Test
