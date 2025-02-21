1090e780cd320d134e11bb478d02ac4fe19b3291
malslapq <malslapq@gmail.com>
2025-02-22 02:52:59 +0900

    test: JwtTokenProviderTest Mockito 기반으로 변경

--- src/test/java/com/kefa/infrastructure/security/jwt/JwtTokenProviderTest.java
+++ src/test/java/com/kefa/infrastructure/security/jwt/JwtTokenProviderTest.java
@@ -6,13 +6,14 @@ import io.jsonwebtoken.MalformedJwtException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class JwtTokenProviderTest {
 
     private JwtTokenProvider jwtTokenProvider;
