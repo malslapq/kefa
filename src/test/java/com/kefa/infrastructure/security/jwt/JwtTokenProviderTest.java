78ae23d89d03ab76b39a5c3ea82d9ed96b98400f
malslapq <malslapq@gmail.com>
2025-02-22 03:12:19 +0900

    test: JwtTokenProviderTest @SpringBootTest 제거

--- src/test/java/com/kefa/infrastructure/security/jwt/JwtTokenProviderTest.java
+++ src/test/java/com/kefa/infrastructure/security/jwt/JwtTokenProviderTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.util.ReflectionTestUtils;
 
@@ -17,7 +16,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest
 public class JwtTokenProviderTest {
 
     @MockBean
