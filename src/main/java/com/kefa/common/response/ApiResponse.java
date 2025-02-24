49e95114c25b1ba28bd8399466185ffa192aca58
malslapq <malslapq@gmail.com>
2025-02-24 15:00:02 +0900

    feat: GlobalExceptionHandler 구현
    - 에러 발생 시 응답할 ApiResponse 추가
    - 컨트롤 할 예외들은 CustomException를 상속 받음
    - JwtTokenProvider에서 JWT 검증 예외 던지는 부분 수정

---
+++ src/main/java/com/kefa/common/response/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.kefa.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private boolean success;
+    private T data;
+    private ErrorResponse error;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return ApiResponse.<T>builder()
+            .success(true)
+            .data(data)
+            .build();
+    }
+
+    public static <T> ApiResponse<T> error(ErrorResponse error) {
+        return ApiResponse.<T>builder()
+            .success(false)
+            .error(error)
+            .build();
+    }
+
+}
