ab0523f2157f5be8d4c99592e8e539bee81459cb
malslapq <malslapq@gmail.com>
2025-02-27 14:22:25 +0900

    chore: SecurityConfigTest @WebMvcTest로 변경

--- src/test/java/com/kefa/infrastructure/security/config/SecurityConfigTest.java
+++ src/test/java/com/kefa/infrastructure/security/config/SecurityConfigTest.java
@@ -1,12 +1,15 @@
 package com.kefa.infrastructure.security.config;
 
+import com.kefa.application.usecase.AuthenticationUseCase;
+import com.kefa.infrastructure.security.auth.OAuth2SuccessHandler;
 import com.kefa.infrastructure.security.jwt.JwtTokenProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -16,8 +19,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(SecurityConfig.class)
 @AutoConfigureMockMvc
+@MockBean(JpaMetamodelMappingContext.class)
 public class SecurityConfigTest {
 
     @Autowired
@@ -26,10 +30,15 @@ public class SecurityConfigTest {
     @MockBean
     private JwtTokenProvider jwtTokenProvider;
 
+    @MockBean
+    private OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @MockBean
+    private AuthenticationUseCase authenticationUseCase;
+
     @Test
     @DisplayName("공개 엔드포인트 접근 가능 성공")
     void publicEndpointsSuccessTest() throws Exception {
-
         List<String> publicUrls = Arrays.asList(
             "/auth/login",
             "/auth/join",
@@ -45,7 +54,6 @@ public class SecurityConfigTest {
     @Test
     @DisplayName("보호된 엔드포인트 접근 실패")
     void protectedEndpointsFailTest() throws Exception {
-
         List<String> protectedUrls = Arrays.asList(
             "/member/profile",
             "/admin/profile",
