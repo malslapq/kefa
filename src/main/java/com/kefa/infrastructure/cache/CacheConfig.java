16a07d24476771a2ca2e362e82622b2aedf6a7fd
malslapq <malslapq@gmail.com>
2025-05-14 21:38:22 +0900

    feat: Caffeine in memory 캐시 설정
    - redis 설정 주석 처리

---
+++ src/main/java/com/kefa/infrastructure/cache/CacheConfig.java
@@ -0,0 +1,20 @@
+package com.kefa.infrastructure.cache;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public Cache<String, String> emailVerificationCache() {
+        return Caffeine.newBuilder()
+            .expireAfterWrite(10, TimeUnit.MINUTES)
+            .build();
+    }
+
+}
